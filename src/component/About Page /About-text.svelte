a0ed869ca1afe88070930049008d7ebf6436e035
Ahmed Onour <ahmedonour.49@gmail.com>
2023-10-10 16:46:59 +0300

    Working on "Our Team" Page

--- src/component/About Page /About-text.svelte
+++ src/component/About Page /About-text.svelte
@@ -1,14 +1,18 @@
 <script>
+
 	export let heading;
 	export let paragraph;
-</script>
-
+	  </script>
 <div class="about-text">
+	
 	<h1>{heading}</h1>
 	<p>{paragraph}</p>
+	
+
 </div>
 
 <style>
+  
 	.about-text {
 		width: var(--Main-width);
 	}
@@ -28,5 +32,24 @@
 		font-style: normal;
 		font-weight: 400;
 		line-height: normal;
+	}
+	@media screen and (min-width: 800px) {
+		.about-text h1 {
+		font-family: var(--ff-DM-Sans);
+		font-weight: 500;
+		font-size: 96px;
+		color: #000;
+
+		line-height: normal;
+	}
+	.about-text p {
+		color: #000;
+		text-align: justify;
+		font-family: var(--ff-Open-Sans);
+		font-size: 40px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: 1.5;
+	}
 	}
 </style>
