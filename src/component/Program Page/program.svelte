816c681dd3dbcb49b4eced19dbd1fe9ec163bb5f
Ahmed Onour <ahmedonour.49@gmail.com>
2023-10-12 09:11:26 +0300

    Adding a program Component

---
+++ src/component/Program Page/program.svelte
@@ -0,0 +1,47 @@
+<script>
+	export let ProgramTitle;
+	export let src;
+	export let alt;
+	export let description;
+</script>
+
+<div class="program">
+	<h1>{ProgramTitle}</h1>
+	<div class="programDetails">
+		<img {src} {alt} />
+		<p>{description}</p>
+	</div>
+</div>
+
+<style>
+	.program {
+		width: 100vw;
+		padding: 1.875rem;
+		display: grid;
+		place-items: center;
+	}
+	.program h1 {
+		color: #000;
+		font-family: var(--ff-DM-Sans);
+		font-size: 1.118375rem;
+		font-style: normal;
+		font-weight: 700;
+		line-height: normal;
+	}
+	.program .programDetails {
+		width: 300px;
+	}
+	.program .programDetails img {
+		width: 100%;
+		margin-block: 2rem;
+	}
+	.program .programDetails p {
+		color: #000;
+		text-align: justify;
+		font-family: var(--ff-Open-Sans);
+		font-size: 15.203px;
+		font-style: normal;
+		font-weight: 400;
+		line-height: normal;
+	}
+</style>
---
+++ src/routes/ourprogram/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Program from "../../component/Program Page/program.svelte";
+
+
+</script>
+<main>
+    <Program 
+    ProgramTitle="Our Programs at Abaqira Academy"
+    src="1.png"
+    alt="an image of apple on a set of books"
+    description="At Abaqira Academy, we are dedicated to providing a comprehensive and enriching educational experience for our students. Our programs are thoughtfully designed to inspire a love for learning, promote academic excellence, and foster holistic development. We believe that every child is unique, and our programs are tailored to meet the diverse needs and talents of our students."
+    />
+</main>
